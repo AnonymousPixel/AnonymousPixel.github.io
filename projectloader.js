10e9eb734d8d67fab13818afc399d3ab380f238a
AnonymousPixel <87269623+AnonymousPixel@users.noreply.github.com>
2022-12-06 13:57:40 +0100

    Update projectloader.js

--- projectloader.js
+++ projectloader.js
@@ -1,3 +1,17 @@
-fetch('./projects/projects.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+let projects = fetch('./projects/projects.json').json()
+let MC = []
+let ST = []
+let GM = []
+
+for (project in projects){
+    switch (project["category"]){
+        case "MC":
+            MC.push(project)
+        case "ST":
+            ST.push(project)
+        case "GM":
+            GM.push(project)
+        default:
+            console.log(project)
+    }
+}
