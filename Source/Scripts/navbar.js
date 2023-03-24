6866b7119d0c047cf00d68350be7ce3388c40cd8
ScottishPuffin <87269623+AnonymousPixel@users.noreply.github.com>
2023-03-24 09:18:45 +0000

    linked w repl

--- Source/Scripts/navbar.js
+++ Source/Scripts/navbar.js
@@ -1,7 +1,8 @@
 function isInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
-      rect.bottom >= document.getElementById('header').getBoundingClientRect().height
+      rect.bottom >= document.getElementById('header').getBoundingClientRect().height &&
+      rect.top <= window.innerHeight
   );
 }
 
@@ -9,10 +10,22 @@ addEventListener("load", (event) => {
   setInterval(loadFunction,10);
 });
 function loadFunction(){
-  if (isInViewport(document.getElementById("se1"))){
-    document.getElementById("s1").style.color = "#73AB84";
+  if (isInViewport(document.getElementById("se5"))){
+    document.getElementById("s1").style.color = "White";
     document.getElementById("s2").style.color = "White";
     document.getElementById("s3").style.color = "White";
+    document.getElementById("s4").style.color = "White";
+    document.getElementById("s5").style.color = "#73AB84";
+  } else if(isInViewport(document.getElementById("se4"))){
+    document.getElementById("s1").style.color = "White";
+    document.getElementById("s2").style.color = "White";
+    document.getElementById("s3").style.color = "White";
+    document.getElementById("s4").style.color = "#73AB84";
+    document.getElementById("s5").style.color = "White";
+  } else if(isInViewport(document.getElementById("se3"))){
+    document.getElementById("s1").style.color = "White";
+    document.getElementById("s2").style.color = "White";
+    document.getElementById("s3").style.color = "#73AB84";
     document.getElementById("s4").style.color = "White";
     document.getElementById("s5").style.color = "White";
   } else if(isInViewport(document.getElementById("se2"))){
@@ -21,23 +34,11 @@ function loadFunction(){
     document.getElementById("s3").style.color = "White";
     document.getElementById("s4").style.color = "White";
     document.getElementById("s5").style.color = "White";
-  } else if(isInViewport(document.getElementById("se3"))){
-    document.getElementById("s1").style.color = "White";
-    document.getElementById("s2").style.color = "White";
-    document.getElementById("s3").style.color = "#73AB84";
-    document.getElementById("s4").style.color = "White";
-    document.getElementById("s5").style.color = "White";
-  } else if(isInViewport(document.getElementById("se4"))){
-    document.getElementById("s1").style.color = "White";
-    document.getElementById("s2").style.color = "White";
-    document.getElementById("s3").style.color = "White";
-    document.getElementById("s4").style.color = "#73AB84";
-    document.getElementById("s5").style.color = "White";
-  } else if(isInViewport(document.getElementById("se5"))){
-    document.getElementById("s1").style.color = "White";
+  } else if(isInViewport(document.getElementById("se1"))){
+    document.getElementById("s1").style.color = "#73AB84";
     document.getElementById("s2").style.color = "White";
     document.getElementById("s3").style.color = "White";
     document.getElementById("s4").style.color = "White";
-    document.getElementById("s5").style.color = "#73AB84";
+    document.getElementById("s5").style.color = "White";
   }
 };
