2de10939806642b49bd5fac8899b36fab3d86352
WeeScottishPuffin <87269623+WeeScottishPuffin@users.noreply.github.com>
2023-03-28 19:08:22 +0200

    Delete Source directory

--- Source/Scripts/year.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("year").innerHTML = new Date().getFullYear()
-document.getElementById("year").style.textDecoration = "none"
-document.getElementById("year").style.fontWeight = "900"
